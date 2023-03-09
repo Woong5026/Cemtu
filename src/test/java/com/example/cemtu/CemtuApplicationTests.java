275d928e2c6b4aff8eef1e4b949ff3054f2bc954
Woong5026 <ktw5026@naver.com>
2023-03-09 23:55:12 +0900

    Initial commit

---
+++ src/test/java/com/example/cemtu/CemtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cemtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemtuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
